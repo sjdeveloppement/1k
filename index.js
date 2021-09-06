c34fc1c00c1489e236717f8fc5fc409e7eab243e
Serra jahan <jahan.serra@gmail.com>
2021-09-06 13:16:49 +0200

    Add javascript tab of object + calcul + result

--- index.js
+++ index.js
@@ -23,8 +23,65 @@ function generateAnswer(){
     spend.classList.add("disabled");
     row.classList.add("disabled");
     
+    // calcul price
+    let obj = [{
+        name : 'GOAT',
+        price : 500,
+        img : './images/goat.jpg'
+    },{
+        name: "Johnny Hallyday's hairs",
+        price : 1800,
+        img : './images/Johnny_hairs.jpg'
+    },
+    {
+        name : "will smith house",
+        price : 42000000,
+        img: './images/smithHouse.jpg'
+    },
+    {
+        name : 'crankshaft',
+        price : 108,
+        img : './images/vilebrequin.jpg'
+    },
+    {
+        name : 'Colonel reyel album',
+        price : 1,
+        img: './images/colonel.jpg'
+    },
+    {
+        name : 'Test PCR positif',
+        price : 46,
+        img :'./images/testpcr.jpg'
+    },
+    {
+        name : 'Thumb ring',
+        price : 12,
+        img: './images/thumbring.jpg'
+    }
+    ]
+    //random choix de l'objet
+    const randomObj = obj[Math.floor(Math.random()*obj.length)];
+    console.log(randomObj);
+    // comparaison du prix de l'objet avec la valeur de l'input
+    const parsed = parseInt(input.value)
+    if(randomObj.price <= parsed){
+       let nbObj= parsed / randomObj.price
+       console.log(Math.round(nbObj));
+       answer.innerText="For : "+ input.value +"€ you can buy "+'X'+' '+ Math.round(nbObj) + ' ' + randomObj.name +' '+ randomObj.price+'€' + '/u';
+       result.innerHTML= `<div><img src='${randomObj.img}'></div>`;
+       
+
+    }else if(randomObj.price > parsed){
+        
+        answer.innerText="For : "+ input.value +"€ you can't buy "+ ' ' + randomObj.name + ' ' +'not enough money for this sorry !'+' '+  'it cost: ' + randomObj.price+'€';
+        result.innerHTML= `<div><img src='${randomObj.img}'></div>`;
+        }
+    else{
+        console.log('rien pour le moment');
+    }
     
-    answer.innerText="For : "+ input.value +"€ you can buy "+ result.value;
+    //
+    //answer.innerText="For : "+ input.value +"€ you can buy "+ result.value;
     result.appendChild(answer);
     setTimeout(()=>{
         answer.remove();
